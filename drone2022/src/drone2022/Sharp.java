3b5526af2d51a676b670ab907942644eccbd4b53
rob <rob@10.0.0.77>
2022-02-26 11:48:46 -0700

    edit

--- drone2022/src/drone2022/Sharp.java
+++ drone2022/src/drone2022/Sharp.java
@@ -1,10 +1,35 @@
 package drone2022;
 
-public class Sharp {
+import java.util.ArrayList;
 
+public class Sharp {
+    private static ArrayList num1(ArrayList num) {
+    	int xyz=0;
+    	num.add(77);
+    	num.add(90);
+    	return num;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("starting now");
+		 ArrayList num = new ArrayList();boolean h=true;
+		 num.add(78);
+		 System.out.println("starting now1: "+num);
+		 num1(num);
+         System.out.println("starting now 2"+num);
+         numbers();
+         this.numbers=new
+         
+	}public static  int numbers() {
+		int checker=9;
+		return checker;
+	}public boolean falseTrue(boolean h) {
+	    if(h==true) {
+	    	System.out.println("Ystem works: ");
+	    }else {
+	    	System.out.println("Ystem doesnt work: ");
+	    }
+		return h;
+		
 	}
 
 }
