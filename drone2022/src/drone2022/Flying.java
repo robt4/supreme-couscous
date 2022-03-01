10822417cf31abc7c6e52e0af4ed47ca57a7f59d
rob <rob@10.0.0.77>
2022-02-28 22:03:22 -0700

    more classes

--- drone2022/src/drone2022/Flying.java
+++ drone2022/src/drone2022/Flying.java
@@ -1,9 +1,47 @@
 package drone2022;
+import java.lang.*;
+import java.util.Objects;
+import java.util.Scanner;
+
+
+
 
 public class Flying {
 	
-   public int canfly() {
-	   System.out.println("Testing "+ nameIt);
-	   return 0;}
-   public String nameIt;
+public int canfly() {                                             
+
+	   System.out.println("Hello "+ num(nameIt));                                        //assigning nameIt as argument 
+	                                                             //Checking if its the user name is true or false
+	   //Scanner 
+	   return 0;
+	}
+   public String nameIt;                                      //object to store myname
+   
+   private String num(String yours) {                      //checking to see if name matches the private data yours
+	   int k=5;
+	   
+	   if((yours.equals("Robert"))||(yours.equals("robert"))){        //can use compareToIgnore() too
+		   return yours;
+	   }else {
+		   throw new IllegalArgumentException();                        //shows error if the name doesnt match
+	   }
+	   
+   }public int call(int x) {                                  //calls the private password checker assigns argument from sharp
+	   
+	  passWord2(x);
+	   return 0;
+   }
+   private int passWord2(int y) {                          //private funtcion to check if password is 1 from sharp class"password can be anythting"
+	   
+	   int x=1;
+	   System.out.println("testing : "+y);
+	   if(x== y) {
+		   return 0;
+		   
+	   }else {
+		   throw new IllegalArgumentException();
+		   
+	   }
+	   
+   }
 }
--- drone2022/src/drone2022/Sharp.java
+++ drone2022/src/drone2022/Sharp.java
@@ -1,9 +1,10 @@
 package drone2022;
-
+//use import statemtnt to import another class from a diff package
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sharp {
-    private static ArrayList num1(ArrayList num) {
+    private static ArrayList num1(ArrayList num) {                       //playing with arraylist
     	int xyz=0;
     	num.add(77);
     	num.add(90);
@@ -15,10 +16,10 @@ public class Sharp {
 		 num.add(78);
 		 System.out.println("starting now1: "+num);
 		 num1(num);
-         System.out.println("starting now 2"+num);
+         System.out.println("starting now 2"+num);                 //tip Syso cltr+spacebar
          numbers();
-         falseTrue(h);
-         
+         falseTrue(h);                                             //calling to start checking password/username
+                  
 	}public static  int numbers() {
 		int checker=9;
 		return checker;
@@ -26,18 +27,24 @@ public class Sharp {
 	
 	public static  boolean falseTrue(boolean h) {
 		int numbrs = 90;int utis=numbers();
-		//this.utis=numbrs;
+		                                                   //creating num object to fill in the class flying with values plus calling e.t.c
 		Flying num=new Flying();
-		num.nameIt="Razor";
-		num.canfly();
-		
-	    if(h==true) {
-	    	System.out.println("Ystem works: ");
-	    }else {
-	    	System.out.println("Ystem doesnt work: ");
-	    }
+		Scanner input=new Scanner(System.in);               //Scanner object
+		System.out.println("Enter your name: ");
+		num.nameIt=input.nextLine();                        //Reading input and assigning it 
+		num.canfly();                                       //object to access function in flying java
+		System.out.println("Enter your password: ");
+	    passWord();
+	    System.out.println("Succesfully login: ");
 		return h;
 		
+	}public static int passWord() {
+		int y=0;
+		Scanner input2=new Scanner(System.in); 
+		y=input2.nextInt();
+		Flying num=new Flying();
+		num.call(y);
+		return y;
 	}
 
 }
